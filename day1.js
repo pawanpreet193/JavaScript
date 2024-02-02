4042e5c29462147d51b5e0c5aaeed85a735d8e45
pawanpreet193 <110889245+pawanpreet193@users.noreply.github.com>
2024-02-02 22:21:16 +0530

    Modified code

--- day1.js
+++ day1.js
@@ -1,1 +1,7 @@
 console.log("hello world")
+let a = "happy"                   //have block level scope , should be used instead of var
+console.log(a)
+console.log("a")
+var fruit = "apple"                   // have global scope , should be avoided.
+console.log(fruit)    
+const pie = 3.14                    // can neither be redeclared or intialised          
