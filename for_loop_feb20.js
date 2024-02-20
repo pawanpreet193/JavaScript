f31c536a22d7063c070b387b243ae8111e231697
pawanpreet193 <110889245+pawanpreet193@users.noreply.github.com>
2024-02-20 18:49:54 +0530

    For loop practice

---
+++ for_loop_feb20.js
@@ -0,0 +1,21 @@
+let num = 10;
+for ( let i = 0; i < num; i++)
+{
+    console.log(2*i);
+}
+
+let obj ={
+    A : 26,
+    B : 23,
+    C : 34,
+    D : 35
+}
+obj.E= 434;
+for( let a in obj){                                //This is a for in loop
+    console.log(a + " " + obj[a]);
+}
+
+
+for( let b  in obj){                                //This is a for in loop
+    console.log("The values of the object are as follows: " + obj[b]);
+}
